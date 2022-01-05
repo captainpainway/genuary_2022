426f3a359c3451a28369eaf925cb3f8363634692
Mary Knize <maryknize@gmail.com>
2022-01-05 07:59:41 -0500

    Day 5 update

--- day_5/script.js
+++ day_5/script.js
@@ -46,13 +46,6 @@ function setup() {
 
 function draw() {
     noFill();
-    // let col = colors[0];
-    // fill(col);
-    // size -= size_desc;
-    // if (frameCount % 50 === 0) {
-    //     colors.shift();
-    //     colors.push(col);
-    // }
     strokeWeight(1);
     stroke(148, 210, 189, 50);
 
@@ -64,36 +57,27 @@ function draw() {
         endShape();
     }
 
-    // if (frameCount > max) {
-    //     console.log("Stopped");
+    strokeWeight(0);
 
-        strokeWeight(0);
-        // fill(0, 0, 0, 255);
-        // rect(0, 0, width, 105);
-        // rect(0, height - 105, width, 105);
-        // rect(0, 0, 105, height);
-        // rect(width - 105, 0, width, height);
+    noFill();
+    stroke(c);
+    strokeWeight(10);
+    strokeCap(SQUARE);
 
-        noFill();
-        stroke(c);
-        strokeWeight(10);
-        strokeCap(SQUARE);
+    // Border
+    line(95, 100, 150, 100); // Top L
+    line(575, 100, width - 95, 100); // Top R
+    line(100, 95, 100, height - 95); // Left
+    line(width - 100, 95, width - 100, height - 95); // Right
+    line(95, height - 100, 880, height - 100); // Bottom L
+    line(1130, height - 100, width - 95, height - 100); // Bottom R
 
-        // Border
-        line(95, 100, 150, 100); // Top L
-        line(575, 100, width - 95, 100); // Top R
-        line(100, 95, 100, height - 95); // Left
-        line(width - 100, 95, width - 100, height - 95); // Right
-        line(95, height - 100, 880, height - 100); // Bottom L
-        line(1130, height - 100, width - 95, height - 100); // Bottom R
+    noStroke();
+    textSize(28);
+    textFont(font);
+    fill(c);
+    text('#Genuary2022, Day 5', 160, 110);
+    text('mary.codes', width - 385, height - 90);
 
-        noStroke();
-        textSize(28);
-        textFont(font);
-        fill(c);
-        text('#Genuary2022, Day 5', 160, 110);
-        text('mary.codes', width - 385, height - 90);
-
-        noLoop();
-    // }
+    noLoop();
 }
